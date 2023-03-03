13f7fe2188dd9d63ba0c98800f86c8f9a88e1d29
JameshaRicketts <jamesharicketts@gmail.com>
2023-03-03 02:34:50 +0000

    took me a WHILE

--- src/main/java/CombineArrays.java
+++ src/main/java/CombineArrays.java
@@ -1,4 +1,3 @@
-
 public class CombineArrays {
     /**
      * Combine two arrays into a single array and return it.
@@ -8,7 +7,21 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
-
-        return null;
+        int[] comboArray = new int[arr1.length + arr2.length];
+        int counter = 0;
+        while (counter < comboArray.length){
+            for(int first = 0; first < arr1.length; first++) {
+                comboArray[counter] = arr1[first];
+                counter++;
+            }
+            
+            for(int second = 0; second < arr1.length; second++) {
+                comboArray[counter] = arr2[second];
+                counter++;
+            }
+            
+        }
+       
+        return comboArray;
     }
 }
